978a4a123eb67800e589bd53c92c422269a7f668
LNA <LAllen@8thLight.com>
2014-10-30 11:01:41 -0400

    Adds all containers for when 1 and else

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,9 +12,9 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "#{number} #{container(number + 1)} of beer on the wall, #{number} #{container(number + 1)} of beer.\nTake it down and pass it around, no more #{container(number)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number)} of beer on the wall.\n"
+      "#{number} #{container(number + 1)} of beer on the wall, #{number} #{container(number + 1)} of beer.\nTake one down and pass it around, #{number-1} #{container(number)} of beer on the wall.\n"
     end
   end
 
