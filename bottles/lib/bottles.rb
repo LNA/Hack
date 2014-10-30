869cc0600bdd6c4a16be47f1dba23e986eb45b09
LNA <LAllen@8thLight.com>
2014-10-30 15:42:09 -0400

    Step 10: Swap out functional methods for object oriented messages

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,9 +9,10 @@ class Bottles
 
   def verse(i)
     number = BottleNumber.new(i)
-    "#{intro(number)} #{container(number + 1)} of beer on the wall, " + 
-    "#{intro(number).downcase} #{container(number + 1)} of beer.\n" + 
-    "#{outro(number)}, #{name_of(number)} #{container(number)} of beer on the wall.\n"
+    next_number = BottleNumber.new(number.number + 1)
+    "#{number.intro} #{next_number.container} of beer on the wall, " + 
+    "#{number.intro.downcase} #{next_number.container} of beer.\n" + 
+    "#{number.outro}, #{number.name_of} #{number.container} of beer on the wall.\n"
   end
 
   private
